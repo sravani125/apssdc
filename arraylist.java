82e72fc0fb3be3278e9c0fa7bcd1234d20d67e84
sravani125 <sravani5a8@gmail.com>
2016-09-21 09:56:04 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+class ArrayListDemo
+{
+public static void main(String args[])
+{
+ArrayList l=new ArrayList();
+//list l=new ArrayList();
+l.add("A");
+l.add("B");
+l.add("C");
+l.add("D");
+l.add("E");
+l.add("F");
+l.add("null");
+System.out.println(l);
+}
+}
+
